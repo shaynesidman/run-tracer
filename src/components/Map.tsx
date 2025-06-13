ae6d15a231ee836c4136844b98dd2ea5e16e8d3d
shaynesidman <147111519+shaynesidman@users.noreply.github.com>
2025-06-13 17:06:38 -0400

    got a basic map up and running

---
+++ src/components/Map.tsx
@@ -0,0 +1,29 @@
+"use client";
+import { useRef, useEffect } from "react";
+import mapboxgl from "mapbox-gl";
+import "mapbox-gl/dist/mapbox-gl.css";
+
+mapboxgl.accessToken = process.env.NEXT_PUBLIC_REACT_APP_MAPBOX_TOKEN;
+
+export default function Map() {
+    const mapContainer = useRef<HTMLDivElement | null>(null);
+    const map = useRef<mapboxgl.Map | null>(null);
+
+    useEffect(() => {
+        if (map.current || !mapContainer.current) return;
+
+        map.current = new mapboxgl.Map({
+            container: mapContainer.current,
+            style: 'mapbox://styles/mapbox/streets-v12',
+            center: [-74.5, 40],
+            zoom: 9
+        })
+    }, [])
+
+    return (
+        <div
+            ref={mapContainer}
+            className="w-[100vw] h-[100vh]"
+        />
+    );
+}
