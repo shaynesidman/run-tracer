00567c6c5e3009310389a754645d83f235590cbf
shaynesidman <147111519+shaynesidman@users.noreply.github.com>
2025-08-15 13:29:18 -0400

    basic navbar implementation

---
+++ src/components/NavBar.tsx
@@ -0,0 +1,11 @@
+export default function NavBar() {
+
+    return (
+        <nav className="w-full flex flex-row justify-between items-center p-4">
+            <h1 className="text-2xl">RunTracer</h1>
+            <div className="flex flex-row justify-center items-center">
+                <h3>Map</h3>
+            </div>
+        </nav>
+    );
+}
