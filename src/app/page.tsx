2d27f4e76c7da19ae3e11b17de956509f1774dd2
shaynesidman <147111519+shaynesidman@users.noreply.github.com>
2025-08-17 12:52:45 -0400

    changed all designs to liquid glass

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,7 +10,7 @@ export default function Home() {
         <section className="bg-fixed bg-gradient-to-br from-green-900 via-green-700 to-gray-600 h-full flex flex-col justify-center items-center gap-6 text-white">
             <p className="text-3xl">Plan and track your runs, walks, bikes, and more with ease.</p>
             <button 
-                className="text-green-900 bg-white text-xl px-6 py-4 rounded-lg hover:cursor-pointer hover:scale-105 transition-transform duration-200"
+                className="text-xl text-center px-6 py-4 rounded-lg bg-white/2.5 border border-white/50 backdrop-blur-sm shadow-[inset_0_1px_0px_rgba(255,255,255,0.75),0_0_9px_rgba(0,0,0,0.2),0_3px_8px_rgba(0,0,0,0.15)] hover:bg-white/20 hover:cursor-pointer before:absolute before:inset-0 before:rounded-lg before:bg-gradient-to-br before:from-white/60 before:via-transparent before:to-transparent before:opacity-70 before:pointer-events-none after:absolute after:inset-0 after:rounded-lg after:bg-gradient-to-tl after:from-white/30 after:via-transparent after:to-transparent after:opacity-50 after:pointer-events-none transition antialiased"
                 onClick={() => router.push("/map")}
             >
                 Go to map
--- src/components/RecentRuns.tsx
+++ src/components/RecentRuns.tsx
@@ -4,13 +4,13 @@ export default function RecentRuns() {
 
     return (
         <div className="flex flex-row items-center gap-6">
-            <div className="bg-white h-72 w-64 rounded-lg">
+            <div className="h-72 w-64 text-xl text-center px-6 py-4 rounded-lg bg-white/2.5 border border-white/50 backdrop-blur-sm shadow-[inset_0_1px_0px_rgba(255,255,255,0.75),0_0_9px_rgba(0,0,0,0.2),0_3px_8px_rgba(0,0,0,0.15)] hover:bg-white/20 hover:cursor-pointer before:absolute before:inset-0 before:rounded-lg before:bg-gradient-to-br before:from-white/60 before:via-transparent before:to-transparent before:opacity-70 before:pointer-events-none after:absolute after:inset-0 after:rounded-lg after:bg-gradient-to-tl after:from-white/30 after:via-transparent after:to-transparent after:opacity-50 after:pointer-events-none transition antialiased">
 
             </div>
-            <div className="bg-white h-72 w-64 rounded-lg">
+            <div className="h-72 w-64 text-xl text-center px-6 py-4 rounded-lg bg-white/2.5 border border-white/50 backdrop-blur-sm shadow-[inset_0_1px_0px_rgba(255,255,255,0.75),0_0_9px_rgba(0,0,0,0.2),0_3px_8px_rgba(0,0,0,0.15)] hover:bg-white/20 hover:cursor-pointer before:absolute before:inset-0 before:rounded-lg before:bg-gradient-to-br before:from-white/60 before:via-transparent before:to-transparent before:opacity-70 before:pointer-events-none after:absolute after:inset-0 after:rounded-lg after:bg-gradient-to-tl after:from-white/30 after:via-transparent after:to-transparent after:opacity-50 after:pointer-events-none transition antialiased">
 
             </div>
-            <div className="bg-white h-72 w-64 rounded-lg">
+            <div className="h-72 w-64 text-xl text-center px-6 py-4 rounded-lg bg-white/2.5 border border-white/50 backdrop-blur-sm shadow-[inset_0_1px_0px_rgba(255,255,255,0.75),0_0_9px_rgba(0,0,0,0.2),0_3px_8px_rgba(0,0,0,0.15)] hover:bg-white/20 hover:cursor-pointer before:absolute before:inset-0 before:rounded-lg before:bg-gradient-to-br before:from-white/60 before:via-transparent before:to-transparent before:opacity-70 before:pointer-events-none after:absolute after:inset-0 after:rounded-lg after:bg-gradient-to-tl after:from-white/30 after:via-transparent after:to-transparent after:opacity-50 after:pointer-events-none transition antialiased">
                 
             </div>
         </div>
