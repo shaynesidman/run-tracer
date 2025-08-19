257e1424411ad83b316b14312730ceed284cf0e5
shaynesidman <147111519+shaynesidman@users.noreply.github.com>
2025-08-19 08:09:23 -0400

    added handling for 0 recent runs

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -16,7 +16,7 @@ export default function NavBar() {
         <nav className="absolute top-2 w-full bg-center rounded-md p-2">
             <div className="w-full inline-flex items-center justify-between px-4 py-2 text-white rounded-lg bg-white/2.5 border border-white/50 backdrop-blur-sm shadow-[inset_0_1px_0px_rgba(255,255,255,0.75),0_0_9px_rgba(0,0,0,0.2),0_3px_8px_rgba(0,0,0,0.15)] before:absolute before:inset-0 before:rounded-lg before:bg-gradient-to-br before:from-white/60 before:via-transparent before:to-transparent before:opacity-70 before:pointer-events-none after:absolute after:inset-0 after:rounded-lg after:bg-gradient-to-tl after:from-white/30 after:via-transparent after:to-transparent after:opacity-50 after:pointer-events-none antialiased">
                 <h1 className="text-2xl hover:cursor-pointer" onClick={() => router.push("/")}>RunTracer</h1>
-                <div className="flex flex-row justify-center items-center gap-4">
+                <div className="flex flex-row justify-center items-center gap-6">
                     <h3 className="hover:cursor-pointer" onClick={() => router.push("/map")}>Map</h3>
                     <SignedOut>
                         <div className="hover:cursor-pointer"><SignInButton /></div>
--- src/components/RecentRuns.tsx
+++ src/components/RecentRuns.tsx
@@ -35,7 +35,7 @@ export default function RecentRuns() {
             hour: 'numeric',
             minute: '2-digit',
             hour12: true
-        }).replace(',', ' at');
+        });
     };
 
     if (!isLoaded) {
@@ -75,13 +75,16 @@ export default function RecentRuns() {
 
     return (
         <div className="text-xl px-6 py-4 rounded-lg bg-white/2.5 border border-white/50 backdrop-blur-sm shadow-[inset_0_1px_0px_rgba(255,255,255,0.75),0_0_9px_rgba(0,0,0,0.2),0_3px_8px_rgba(0,0,0,0.15)] hover:bg-white/20 hover:cursor-pointer before:absolute before:inset-0 before:rounded-lg before:bg-gradient-to-br before:from-white/60 before:via-transparent before:to-transparent before:opacity-70 before:pointer-events-none after:absolute after:inset-0 after:rounded-lg after:bg-gradient-to-tl after:from-white/30 after:via-transparent after:to-transparent after:opacity-50 after:pointer-events-none transition antialiased">
-            {recentRuns.map(run => (
+            {recentRuns.length > 0 && recentRuns.map(run => (
                 <div key={run.id} className="flex flex-col">
                     <p>{run.type}</p>
                     <p>{formatDate(run.time)}</p>
                     <p>{run.distance.toFixed(2)} mi</p>
                 </div>
             ))}
+            {recentRuns.length == 0 && (
+                <p>Go to map to start tracking runs.</p>
+            )}
         </div>
     ); 
 }
