dcb8ab90394a51b51717536878b6b0159aa7ed98
shaynesidman <147111519+shaynesidman@users.noreply.github.com>
2025-08-25 16:49:25 -0400

    updated about page

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -2,8 +2,10 @@
 export default function About() {
     return (
         <section className="text-white max-w-4xl mx-auto p-4 flex flex-col gap-4">
+            <h3 className="font-bold text-xl">RunTracer</h3>
             <p>RunTracer is an application intended for users to track any form of activity and travel, including, but not limited to, running, walking, biking, and driving.</p>
             <p>Users can save their route history by signing in or signing up using the appropriate buttons on the navigation bar.</p>
+            <h3 className="font-bold text-xl mt-4">How to use</h3>
             <p>On the map, there are three modes: Click mode, Route mode, and Draw mode.</p>
             <p>In <span className="font-bold">click mode</span>, every click is a point that connects to the previous one and the total distance is displayed. This allows for a route to be a series of straight, connected lines.</p>
             <p>In <span className="font-bold">route mode</span>, users enter a desired distance, and a route is automatically prepared for them.</p>
