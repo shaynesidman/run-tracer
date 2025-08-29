cbcb9366b0546ca698391a09c9f33436c5efe537
shaynesidman <147111519+shaynesidman@users.noreply.github.com>
2025-08-29 19:14:49 -0400

    fixed navbar styling on small screens

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -14,9 +14,10 @@ export default function NavBar() {
 
     return (
         <nav className="sticky top-2 z-10 w-full bg-center rounded-md p-2">
-            <div className="w-full inline-flex items-center justify-between px-4 py-2 text-white rounded-lg bg-white/2.5 border border-white/50 backdrop-blur-sm shadow-[inset_0_1px_0px_rgba(255,255,255,0.75),0_0_9px_rgba(0,0,0,0.2),0_3px_8px_rgba(0,0,0,0.15)] before:absolute before:inset-0 before:rounded-lg before:bg-gradient-to-br before:from-white/60 before:via-transparent before:to-transparent before:opacity-70 before:pointer-events-none after:absolute after:inset-0 after:rounded-lg after:bg-gradient-to-tl after:from-white/30 after:via-transparent after:to-transparent after:opacity-50 after:pointer-events-none antialiased">
-                <h1 className="text-2xl hover:cursor-pointer" onClick={() => router.push("/")}>RunTracer</h1>
-                <div className="flex flex-row justify-center items-center gap-2 sm:gap-6">
+            <div className="w-full inline-flex items-center justify-center sm:justify-between px-4 py-2 text-white rounded-lg bg-white/2.5 border border-white/50 backdrop-blur-sm shadow-[inset_0_1px_0px_rgba(255,255,255,0.75),0_0_9px_rgba(0,0,0,0.2),0_3px_8px_rgba(0,0,0,0.15)] before:absolute before:inset-0 before:rounded-lg before:bg-gradient-to-br before:from-white/60 before:via-transparent before:to-transparent before:opacity-70 before:pointer-events-none after:absolute after:inset-0 after:rounded-lg after:bg-gradient-to-tl after:from-white/30 after:via-transparent after:to-transparent after:opacity-50 after:pointer-events-none antialiased">
+                <div className="hidden sm:block"><h1 className="text-2xl hover:cursor-pointer" onClick={() => router.push("/")}>RunTracer</h1></div>
+                <div className="flex flex-row justify-center items-center gap-4 sm:gap-6">
+                    <div className="block sm:hidden"><h3 className="hover:cursor-pointer" onClick={() => router.push("/")}>Home</h3></div>
                     <h3 className="hover:cursor-pointer" onClick={() => router.push("/map")}>Map</h3>
                     <h3 className="hover:cursor-pointer" onClick={() => router.push("/about")}>About</h3>
                     <h3 className="hover:cursor-pointer" onClick={() => router.push("/account")}>Account</h3>
