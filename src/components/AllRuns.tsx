9d4280a13504d03d8bdc12ec65af1a8e32334635
shaynesidman <147111519+shaynesidman@users.noreply.github.com>
2025-08-29 16:26:42 -0400

    moved signed in check to be default for recent and all runs

--- src/components/AllRuns.tsx
+++ src/components/AllRuns.tsx
@@ -10,6 +10,15 @@ export default function AllRuns() {
 
     const { userId, isLoaded } = useAuth();
 
+    // User is not logged in
+    if (!userId) {
+        return (
+            <div className="text-xl text-center px-6 py-4 rounded-lg bg-white/2.5 border border-white/50 backdrop-blur-sm shadow-[inset_0_1px_0px_rgba(255,255,255,0.75),0_0_9px_rgba(0,0,0,0.2),0_3px_8px_rgba(0,0,0,0.15)] before:absolute before:inset-0 before:rounded-lg before:bg-gradient-to-br before:from-white/60 before:via-transparent before:to-transparent before:opacity-70 before:pointer-events-none after:absolute after:inset-0 after:rounded-lg after:bg-gradient-to-tl after:from-white/30 after:via-transparent after:to-transparent after:opacity-50 after:pointer-events-none transition antialiased">
+                Sign in to see recent activity.
+            </div>
+        );
+    }
+
     useEffect(() => {
         if (isLoaded && userId) fetchActivities();
     }, [userId, isLoaded]);
@@ -67,15 +76,6 @@ export default function AllRuns() {
                         d="m4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
                     />
                 </svg>
-            </div>
-        );
-    }
-
-    // User is not logged in
-    if (!userId) {
-        return (
-            <div className="text-xl text-center px-6 py-4 rounded-lg bg-white/2.5 border border-white/50 backdrop-blur-sm shadow-[inset_0_1px_0px_rgba(255,255,255,0.75),0_0_9px_rgba(0,0,0,0.2),0_3px_8px_rgba(0,0,0,0.15)] before:absolute before:inset-0 before:rounded-lg before:bg-gradient-to-br before:from-white/60 before:via-transparent before:to-transparent before:opacity-70 before:pointer-events-none after:absolute after:inset-0 after:rounded-lg after:bg-gradient-to-tl after:from-white/30 after:via-transparent after:to-transparent after:opacity-50 after:pointer-events-none transition antialiased">
-                Sign in to see recent activity.
             </div>
         );
     }
