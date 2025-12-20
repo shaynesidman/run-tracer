66cbf32a012710bdbf777a0ae919246d5350b0ce
shaynesidman <147111519+shaynesidman@users.noreply.github.com>
2025-12-20 13:17:52 -0500

    added basic content to popup

--- src/components/ActivityModal.tsx
+++ src/components/ActivityModal.tsx
@@ -1,8 +1,11 @@
 import { createPortal } from "react-dom";
 import { useState, useEffect } from "react";
 import { Activity } from "@/types/activity";
+import { IoIosClose } from "react-icons/io";
+import MiniMap from "./MiniMap";
+import ImageUpload from "./ImageUpload";
 
-export default function ActivityModal({ isOpen, onClose, activity}: { isOpen: boolean; onClose: () => void; activity: Activity }) {
+export default function ActivityModal({ isOpen, onClose, activity}: { isOpen: boolean; onClose: () => void; activity: Activity }) {    
     if (!isOpen) return null;
 
     return createPortal(
@@ -14,8 +17,17 @@ export default function ActivityModal({ isOpen, onClose, activity}: { isOpen: bo
                 className="flex flex-col items-center justify-center min-h-screen"
                 onClick={(e) => e.stopPropagation()}
             >
-                <div className="bg-var(--bg-secondary) p-8 rounded-lg max-w-xl mx-4">
-
+                <div className="bg-[var(--bg-secondary)] rounded-lg min-w-md max-w-lg mx-4 flex flex-col gap-2 px-2 pb-2">
+                    <div className="pt-2 self-end hover:cursor-pointer" onClick={onClose}><IoIosClose /></div>
+                    <div className="flex gap-4"> 
+                        <div className="flex flex-col">
+                            <p>{activity.type}</p>
+                            <p>{activity.time}</p>
+                            <p>{activity.distance}</p>
+                        </div>
+                        <MiniMap activity={activity} />
+                    </div>
+                    <ImageUpload />
                 </div>
             </div>
         </div>,
---
+++ src/components/ImageUpload.tsx
@@ -0,0 +1,7 @@
+export default function ImageUpload() {
+    return (
+        <div className="border border-black rounded-lg px-2">
+            <input type="file" accept="image/*" />
+        </div>
+    );
+}
