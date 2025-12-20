41fb01ffd72677dc3f67d283c9758c26ab8e1400
shaynesidman <147111519+shaynesidman@users.noreply.github.com>
2025-12-20 13:34:43 -0500

    fixed error with activity card disappearing when modal is open

--- src/components/ActivityCard.tsx
+++ src/components/ActivityCard.tsx
@@ -24,29 +24,28 @@ export default function ActivityCard({ activity }: { activity: Activity }) {
         });
     };
 
-    if (isModalOpen) {
-        return <ActivityModal 
-            isOpen={isModalOpen} 
-            onClose={() => setIsModalOpen(false)} 
-            activity={activity} 
-        />;
-    }
-
     return (
-        <div 
-            key={activity.id} 
-            className="flex justify-center items-center rounded-lg gap-4 px-4 py-2 mb-4 border border-[var(--bg-secondary)] hover:cursor-pointer hover:bg-[var(--bg-secondary)] duration-300"
-            onClick={() => setIsModalOpen(true)}
-        >
-            <div className="flex flex-col">
-                <p>{activity.type}</p>
-                <p>{formatDate(activity.time)}</p>
-                <p>{formatTime(activity.time)}</p>
-                <p>{activity.distance.toFixed(2)} miles</p>
+        <>
+            <div 
+                key={activity.id} 
+                className="flex justify-center items-center rounded-lg gap-4 px-4 py-2 mb-4 border border-[var(--bg-secondary)] hover:cursor-pointer hover:bg-[var(--bg-secondary)] duration-300"
+                onClick={() => setIsModalOpen(true)}
+            >
+                <div className="flex flex-col">
+                    <p>{activity.type}</p>
+                    <p>{formatDate(activity.time)}</p>
+                    <p>{formatTime(activity.time)}</p>
+                    <p>{activity.distance.toFixed(2)} miles</p>
+                </div>
+                <div className="w-32 h-32">
+                    <MiniMap activity={activity} />
+                </div>
             </div>
-            <div className="w-32 h-32">
-                <MiniMap activity={activity} />
-            </div>
-        </div>
+            {isModalOpen && <ActivityModal 
+                isOpen={isModalOpen} 
+                onClose={() => setIsModalOpen(false)} 
+                activity={activity} 
+            />}
+        </>
     );
 }
