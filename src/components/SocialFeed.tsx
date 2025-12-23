d70c3b0ae9d7b681878db172261876dc28ff9020
shaynesidman <147111519+shaynesidman@users.noreply.github.com>
2025-12-23 15:17:39 -0500

    added api call to social feed

--- src/components/SocialFeed.tsx
+++ src/components/SocialFeed.tsx
@@ -1,5 +1,29 @@
+"use client";
+
+import { useState, useEffect } from "react";
+import { handleAPIResponse } from "@/lib/apiClient";
+import { type Activity } from "@/types/activity";
+
 export default function SocialFeed() {
-    
+    const [activities, setActivities] = useState<Activity[]>([]);
+
+    useEffect(() => {
+        const fetchActivities = async () => {
+            try {
+                const res = await fetch("/api/fetch/activities", {
+                    method: "GET",
+                    headers: { "Content-Type": "application/json" }
+                });
+
+                const data = await handleAPIResponse<{ data: Activity[] }>(res);
+                setActivities(data.data);
+            } catch (error) {
+                console.log(error);
+            }
+        };
+
+        fetchActivities();
+    }, []);
 
     return (
         <></>
