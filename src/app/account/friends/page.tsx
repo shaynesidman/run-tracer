d7e467ddff71674bb8b656013e646330f64e96ab
shaynesidman <147111519+shaynesidman@users.noreply.github.com>
2026-01-06 17:46:02 -0500

    added shadcn and downloaded table component

--- src/app/account/friends/page.tsx
+++ src/app/account/friends/page.tsx
@@ -1,6 +1,6 @@
 export default function FriendsPage() {
     return (
-        <>
+        <div className="w-full px-2">
             <section>
                 <h2 className="self-start text-xl font-bold">Friends</h2>
             </section>
@@ -10,6 +10,6 @@ export default function FriendsPage() {
             <section>
                 <h2 className="self-start text-xl font-bold">Incoming Friend Requests</h2>
             </section>
-        </>
+        </div>
     );
 }
