6ee413b4b8099abe4ee5e7fad0b536a3adc91d29
shaynesidman <147111519+shaynesidman@users.noreply.github.com>
2026-01-14 22:23:59 -0500

    fixed width issues

--- src/app/feed/page.tsx
+++ src/app/feed/page.tsx
@@ -7,7 +7,7 @@ export default function FeedPage() {
     const [isLoading, setIsLoading] = useState(true);
 
     return (
-        <section className="max-w-5xl mx-4 p-4 flex flex-col gap-4">
+        <section className="w-full max-w-5xl mx-4 p-4 flex flex-col gap-4">
             {!isLoading && <h3 className="self-start text-xl font-bold">Recent Posts</h3>}
             <SocialFeed onLoadingChange={setIsLoading} />
         </section>
